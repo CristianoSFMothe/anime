6cd10c69153b631ee4134dd670cc645670ae4896
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-03-13 23:07:07 -0300

    feat: add react toast

--- src/context/global.js
+++ src/context/global.js
@@ -1,4 +1,6 @@
-import React, {createContext, useContext, useReducer} from "react";
+import React, { createContext, useContext, useReducer } from "react";
+import { toast, ToastContainer } from 'react-toastify'; // Importa o Toastify
+import 'react-toastify/dist/ReactToastify.css'; // Importa o CSS do Toastify
 
 const GlobalContext = createContext();
 
@@ -14,28 +16,27 @@ const GET_PICTURES = "GET_PICTURES";
 
 //reducer
 const reducer = (state, action) => {
-    switch(action.type){
+    switch (action.type) {
         case LOADING:
-            return {...state, loading: true}
+            return { ...state, loading: true }
         case GET_POPULAR_ANIME:
-            return {...state, popularAnime: action.payload, loading: false}
+            return { ...state, popularAnime: action.payload, loading: false }
         case SEARCH:
-            return {...state, searchResults: action.payload, loading: false}
+            return { ...state, searchResults: action.payload, loading: false }
         case GET_UPCOMING_ANIME:
-            return {...state, upcomingAnime: action.payload, loading: false}
+            return { ...state, upcomingAnime: action.payload, loading: false }
         case GET_AIRING_ANIME:
-            return {...state, airingAnime: action.payload, loading: false}
+            return { ...state, airingAnime: action.payload, loading: false }
         case GET_PICTURES:
-            return {...state, pictures: action.payload, loading: false}
+            return { ...state, pictures: action.payload, loading: false }
         default:
             return state;
     }
 }
 
-
-export const GlobalContextProvider = ({children}) => {
-    //intial state
-    const intialState = {
+export const GlobalContextProvider = ({ children }) => {
+    //initial state
+    const initialState = {
         popularAnime: [],
         upcomingAnime: [],
         airingAnime: [],
@@ -45,70 +46,68 @@ export const GlobalContextProvider = ({children}) => {
         loading: false,
     }
 
-    const [state, dispatch] = useReducer(reducer, intialState);
+    const [state, dispatch] = useReducer(reducer, initialState);
     const [search, setSearch] = React.useState('');
 
 
     //handle change
     const handleChange = (e) => {
         setSearch(e.target.value);
-        if(e.target.value === ''){
+        if (e.target.value === '') {
             state.isSearch = false;
         }
     }
 
-
     //handle submit
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(search){
+        if (search) {
             searchAnime(search);
             state.isSearch = true;
-        }else{
+        } else {
             state.isSearch = false;
-            alert('Please enter a search term')
+            toast.error('Please enter a search term'); // Substitui alert por Toastify
         }
     }
 
     //fetch popular anime
     const getPopularAnime = async () => {
-        dispatch({type: LOADING})
+        dispatch({ type: LOADING })
         const response = await fetch(`${baseUrl}/top/anime?filter=bypopularity`);
         const data = await response.json();
-        dispatch({type: GET_POPULAR_ANIME, payload: data.data})
+        dispatch({ type: GET_POPULAR_ANIME, payload: data.data })
     }
 
     //fetch upcoming anime
     const getUpcomingAnime = async () => {
-        dispatch({type: LOADING})
+        dispatch({ type: LOADING })
         const response = await fetch(`${baseUrl}/top/anime?filter=upcoming`);
         const data = await response.json();
-        dispatch({type: GET_UPCOMING_ANIME, payload: data.data})
+        dispatch({ type: GET_UPCOMING_ANIME, payload: data.data })
     }
-
 
     //fetch airing anime
     const getAiringAnime = async () => {
-        dispatch({type: LOADING})
+        dispatch({ type: LOADING })
         const response = await fetch(`${baseUrl}/top/anime?filter=airing`);
         const data = await response.json();
-        dispatch({type: GET_AIRING_ANIME, payload: data.data})
+        dispatch({ type: GET_AIRING_ANIME, payload: data.data })
     }
 
     //search anime
     const searchAnime = async (anime) => {
-        dispatch({type: LOADING})
+        dispatch({ type: LOADING })
         const response = await fetch(`https://api.jikan.moe/v4/anime?q=${anime}&order_by=popularity&sort=asc&sfw`);
         const data = await response.json();
-        dispatch({type: SEARCH, payload: data.data})
+        dispatch({ type: SEARCH, payload: data.data })
     }
 
     //get anime pictures
     const getAnimePictures = async (id) => {
-        dispatch({type: LOADING})
+        dispatch({ type: LOADING })
         const response = await fetch(`https://api.jikan.moe/v4/characters/${id}/pictures`);
         const data = await response.json();
-        dispatch({type: GET_PICTURES, payload: data.data})
+        dispatch({ type: GET_PICTURES, payload: data.data })
     }
 
     //initial render
@@ -116,7 +115,7 @@ export const GlobalContextProvider = ({children}) => {
         getPopularAnime();
     }, [])
 
-    return(
+    return (
         <GlobalContext.Provider value={{
             ...state,
             handleChange,
@@ -126,9 +125,10 @@ export const GlobalContextProvider = ({children}) => {
             getPopularAnime,
             getUpcomingAnime,
             getAiringAnime,
-            getAnimePictures 
+            getAnimePictures
         }}>
             {children}
+            <ToastContainer /> {/* Coloca o ToastContainer aqui */}
         </GlobalContext.Provider>
     )
 }
